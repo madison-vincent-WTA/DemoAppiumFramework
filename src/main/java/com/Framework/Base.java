04219114f014aa7a92c09c34066663124ba0fe55
madison-vincent <madison.vincent.work@gmail.com>
2021-10-15 11:02:40 -0600

    Adding commentary and TODO to the Base class

--- src/main/java/com/Framework/Base.java
+++ src/main/java/com/Framework/Base.java
@@ -25,9 +25,9 @@ public class Base {
     public static boolean checkIfServerIsRunning(int port) {
         boolean isServerRunning = false;
         ServerSocket serverSocket;
+        //Checking to see if the Appium server is running and returning a true or false value
         try {
             serverSocket = new ServerSocket(port);
-
             serverSocket.close();
         } catch (IOException e) {
             isServerRunning = true;
@@ -37,57 +37,8 @@ public class Base {
         return isServerRunning;
     }
 
-    public static void startEmulator() throws IOException, InterruptedException {
-        Runtime.getRuntime().exec(System.getProperty("user.dir")+"/src/main/resources/startEmulator.bat");
-        Thread.sleep(6000);
-    }
-
-//    if (device.contains("emulator"))
-//    {
-//        startEmulator();
-//        // IF the device name in Global Properties contains the word "emulator", we will start the emulator
-//        //This requires you to change the naming convention in Android Virtual Devices
-//        // TODO add this to a beforetest?
-//        //TODO add to a separate method
-//        //TODO update device names for physical / emulator and comment accordingly
-//        //TODO add If/Then logic or an AfterTest that shuts down the emulator
-//    }
-
-    public static void getScreenshot(String testName) throws IOException {
-        File scrFile=	((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(scrFile,new File (System.getProperty("user.dir")+"/test-output/Screenshots/"+testName+".png"));
-        File destFile = new File(System.getProperty("user.dir")+"/test-output/Screenshots/"+testName+".png");
-        FileUtils.copyFile(scrFile, destFile);
-//        Reporter.log("<a href='/DemoFramework/test-output/Screenshots/"+testName+".png'> <img src='/DemoFramework/test-output/Screenshots/"+testName+".png' height='400' width='200'/> </a>");
-        System.out.println("***** Screenshot Captured *****");
-        //TODO - see if its possible to add this to Extent Reports as well...
-
-        FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/Framework/global.properties");
-        Properties prop=new Properties();
-        prop.load(fis);
-        String project=(String) prop.get("projectName");
-        Reporter.log("<a href='/"+project+"/test-output/Screenshots/"+testName+".png'> <img src='/"+project+"/test-output/Screenshots/"+testName+".png' height='400' width='200'/> </a>");
-    }
-
-    public static WebElement waitForElement(WebElement element, int timeOutSec, int pollingSec) {
-        FluentWait<WebDriver> fWait = new FluentWait<WebDriver>(driver)
-                .withTimeout(Duration.ofSeconds(timeOutSec))
-                .pollingEvery(Duration.ofMillis(pollingSec));
-
-        for (int i = 0; i < 2; i++) {
-            try {
-                fWait.until(ExpectedConditions.visibilityOf(element));
-                fWait.until(ExpectedConditions.elementToBeClickable(element));
-            } catch (Exception e) {
-
-                System.out.println("Element Not found trying again - " + element.toString().substring(70));
-                e.printStackTrace();
-            }
-        }
-        return element;
-    }
-
     public AppiumDriverLocalService startServer() {
+        //Checking if the Appium server is running and starting it if it is not
         boolean flag=	checkIfServerIsRunning(4723);
         if(!flag)
         {
@@ -98,6 +49,7 @@ public class Base {
     }
 
     public AppiumDriverLocalService stopServer() {
+        //Checking if the Appium server is running and stopping it if it is
         boolean flag=	checkIfServerIsRunning(4723);
         if(flag)
         {
@@ -105,5 +57,71 @@ public class Base {
             service.stop();
         }
         return service;
+    }
+
+    public static void startEmulator() throws IOException, InterruptedException {
+        //Starting the emulator
+        //Defining the path and file that will be used to start the emulator via the Terminal
+        Runtime.getRuntime().exec(System.getProperty("user.dir")+"/src/main/resources/startEmulator.bat");
+        //TODO Update startEmulator.bat to use Global Properties
+        //Forcing a 6 second wait to allow the emulator to load
+        Thread.sleep(6000);
+    }
+
+//    if (device.contains("emulator"))
+//    {
+//        startEmulator();
+//        // IF the device name in Global Properties contains the word "emulator", we will start the emulator
+//        //This requires you to change the naming convention in Android Virtual Devices
+//        //TODO add this to a BeforeTest?
+//        //TODO add to a separate method
+//        //TODO update device names for physical / emulator and comment accordingly
+//        //TODO add If/Then logic or an AfterTest that shuts down the emulator
+//    }
+
+    public static void getScreenshot(String testName) throws IOException {
+        //Capturing a screenshot and saving it in the test-output/Screenshots folder in the project. It is also logged to the Emailable-Report in test-output
+        //Take a screenshot and save it as a file
+        File scrFile=	((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        //Copy the screenshot and save it as a file in the project directory
+        FileUtils.copyFile(scrFile,new File (System.getProperty("user.dir")+"/test-output/Screenshots/"+testName+".png"));
+
+        // Creates a FileInputStream by opening a connection to an actual file, the file named by the path name in the file system
+        FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/Framework/global.properties");
+        // Creates an empty property list with no default values.
+        Properties prop=new Properties();
+        //Reads a property list (key and element pairs) from the input stream.
+        prop.load(fis);
+        //Retrieving the project name from Global Properties and storing it as a string
+        String project=(String) prop.get("projectName");
+
+        //Adding the screenshot to emailable-report.html in test-output folder.
+        //Project string is used to define the image location as a 404 error is returned without this
+        Reporter.log("<a href='/"+project+"/test-output/Screenshots/"+testName+".png'> <img src='/"+project+"/test-output/Screenshots/"+testName+".png' height='400' width='200'/> </a>");
+
+        //Logging that a screenshot was captured in test output
+        System.out.println("***** Screenshot Captured *****");
+        //TODO - see if its possible to add this to Extent Reports as well...
+    }
+
+    public static WebElement waitForElement(WebElement element, int timeOutSec, int pollingSec) {
+        //Waiting for an element to appear with a defined timeout and polling time
+        //Setting up the FluentWait
+        FluentWait<WebDriver> fWait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(timeOutSec))
+                .pollingEvery(Duration.ofMillis(pollingSec));
+        //Waiting for the element to be visible and interactable
+        for (int i = 0; i < 2; i++) {
+            try {
+                fWait.until(ExpectedConditions.visibilityOf(element));
+                fWait.until(ExpectedConditions.elementToBeClickable(element));
+            } catch (Exception e) {
+                //Logging information for errors
+                System.out.println("Element Not found trying again - " + element.toString().substring(70));
+                e.printStackTrace();
+            }
+        }
+        //Returning the element for use in other methods
+        return element;
     }
 }
