711a34f1e9bc7c4b5cabdb66bc9464a6c36d57e3
madison-vincent <madison.vincent.work@gmail.com>
2021-10-15 11:19:53 -0600

    Adding commentary and TODO to the AndroidTests class

--- src/test/java/AndroidTests.java
+++ src/test/java/AndroidTests.java
@@ -21,9 +21,15 @@ public class AndroidTests extends AndroidBase {
     @BeforeTest
     public void beforeTest() throws IOException, InterruptedException
     {
+        //Starting the Appium server if it is not already running
         service=startServer();
+        //TODO change this to BeforeSuite?
+        //Initializing the driver and supplying the application name
         AndroidDriver<AndroidElement> driver=capabilities("apiDemo");
+        //TODO: Should this be removed from the base if it lives in the beforeTest? Or should the beforeTest itself be moved to Base?
+        //TODO: Application name should only be supplied by Global Properties
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //TODO: Supply helpful information on the above and it's function
 
         // TODO adding an afterTest (?) that also stops the server via lesson 105.. Thinking it needs to
         //  be in base if possible
@@ -34,31 +40,31 @@ public class AndroidTests extends AndroidBase {
 
     @AfterTest
     public void afterTest() {
+        //Stopping the server if it is running, to ensure we dont run into any issues
         service=stopServer();
-        //TODO additional tests to test this functionality
+        //TODO Change this to AfterSuite instead
     }
 
-    //Note: The below parameterized test must be run from the XML file due to parameter dependencies
-    @Parameters({"URL"}) // TODO: needs to be added as a string in the test method and used in the test
+    // Below is a great example of using descriptions, groups, enabling/disabling, page objects and data providers
+//    //Note: The below parameterized test must be run from the XML file due to parameter dependencies
+//    @Parameters({"URL"}) // TODO: needs to be added as a string in the test method and used in the test
     @Test (groups= {"smoke"}, enabled = true, dataProvider= "ClickTestData",dataProviderClass= HomePageData.class,
     description = "Both buttons on the home screen lead to the correct page")
-    public void checkButtonFunction(String buttonName, String desc, String buttonText, String message) throws InterruptedException {
-        Thread.sleep(3000);
+    public void checkButtonFunction(String buttonName, String desc, String buttonText, String message) throws InterruptedException, MalformedURLException {
+        //Searching for an element and clicking on it, based on the xpath which is partially supplied by the data provider
         WebElement element = driver.findElementByXPath("//android.widget.TextView[@content-desc='"+buttonName+"']");
         element.click();
-        Thread.sleep(3000);
-        try {
-            softAssert.assertEquals(android.getButtonText(desc), buttonText, message);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        //Checking that the existing button text is the same as the expected text, and supplying a message for the Assertion
+        //In this example, the message = "Checking Accessibility button text"
+        //If the assertion fails it will print "Assertion: Checking animation button text <FAILED> "
+        softAssert.assertEquals(android.getButtonText(desc), buttonText, message);
+        //Pressing the "back" button on the device to return to the home screen
         driver.pressKey(new KeyEvent(AndroidKey.BACK));
-        Thread.sleep(3000);
+        //TODO: Creating this as an AfterTest to ensure we don't cause issues with screenshots
+
+        //Causes an exception to be thrown if any assertions fail, failing the test and printing information on the failure
+        //This should be included in any method that contains assertions. SoftAssert is preferred over regular assertions
         softAssert.assertAll();
     }
-// Above is a great example of using both page objects and data providers
-
-
-
 }
 
